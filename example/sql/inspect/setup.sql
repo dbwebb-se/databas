89bde13b21d9f6b0d9acdd28ad2b5205dde176c0
Mikael Roos <mikael.t.h.roos@gmail.com>
2018-01-17 16:12:34 +0100

    fix create database

--- example/sql/inspect/setup.sql
+++ example/sql/inspect/setup.sql
@@ -3,7 +3,7 @@
 --
 CREATE DATABASE IF NOT EXISTS skolan;
 GRANT ALL ON skolan.* TO user@localhost IDENTIFIED BY 'pass';
-GRANT FILE ON *.* TO TO user@localhost IDENTIFIED BY 'pass';
+GRANT FILE ON *.* TO user@localhost IDENTIFIED BY 'pass';
 
 CREATE DATABASE IF NOT EXISTS dbwebb;
 GRANT ALL ON dbwebb.* TO user@localhost IDENTIFIED BY 'pass';
