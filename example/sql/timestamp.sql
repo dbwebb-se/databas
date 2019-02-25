a7e2ecbbef8b152868786e6f8ddf9f5f7a70b8d2
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-02-25 11:55:04 +0100

    Update example on timestamp with a forum thread

--- example/sql/timestamp.sql
+++ example/sql/timestamp.sql
@@ -1,9 +1,24 @@
 --
 -- Showing off timestamps.
 --
+DROP TABLE IF EXISTS t1;
+
 CREATE TABLE t1 (
+    id CHAR(2) PRIMARY KEY,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                             ON UPDATE CURRENT_TIMESTAMP
-    deleted TIMESTAMP DEFAULT NULL,
+                             ON UPDATE CURRENT_TIMESTAMP,
+    deleted TIMESTAMP DEFAULT NULL
 );
+
+INSERT INTO t1 (id) VALUES
+    (1), (2), (3);
+
+SELECT * FROM t1;
+
+UPDATE t1 SET id = 11 WHERE id = 1;
+SELECT * FROM t1;
+
+UPDATE t1 SET deleted = NOW() WHERE id = 11;
+SELECT * FROM t1;
+SELECT * FROM t1 WHERE deleted IS NULL;
