6eaf4fd16ab2833e0fcf094503c8e5a5edf1959d
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-02-25 14:01:16 +0100

    Add comments to timestamp

--- example/sql/timestamp.sql
+++ example/sql/timestamp.sql
@@ -11,14 +11,23 @@ CREATE TABLE t1 (
     deleted TIMESTAMP DEFAULT NULL
 );
 
+--
+-- Add some rows
+--
 INSERT INTO t1 (id) VALUES
     (1), (2), (3);
 
 SELECT * FROM t1;
 
+--
+-- Show off how ON UPDATE works
+--
 UPDATE t1 SET id = 11 WHERE id = 1;
 SELECT * FROM t1;
 
+--
+-- Show the concept of "soft delete".
+--
 UPDATE t1 SET deleted = NOW() WHERE id = 11;
 SELECT * FROM t1;
 SELECT * FROM t1 WHERE deleted IS NULL;
