fb6176e479ebf2724038049f296eb1a1d31935c4
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-02-25 16:28:17 +0100

    Walkthrough of functions and index samples

--- example/sql/function.sql
+++ example/sql/function.sql
@@ -1,117 +1,160 @@
 --
--- Example
+-- Example on functions.
 --
-DROP TABLE IF EXISTS Exam;
-CREATE TABLE Exam
+DROP TABLE IF EXISTS exam;
+CREATE TABLE exam
 (
-	`acronym` CHAR(4) PRIMARY KEY,
+    `acronym` CHAR(4) PRIMARY KEY,
     `score` INTEGER
 );
 
-INSERT INTO Exam
+INSERT INTO exam
 VALUES
-	("adam", 77),
-    ("ubbe", 52),
-    ("june", 49),
-    ("john", 63),
-    ("meta", 97),
-    ("siva", 88);
+    ('adam', 77),
+    ('ubbe', 52),
+    ('june', 49),
+    ('john', 63),
+    ('meta', 97),
+    ('siva', 88);
 
-SELECT * FROM Exam;
+SELECT * FROM exam;
 
 
 
 --
 -- Function for grading an exam.
 --
-DELIMITER //
+DROP FUNCTION IF EXISTS grade;
+DELIMITER ;;
 
-DROP FUNCTION IF EXISTS Grade //
-CREATE FUNCTION Grade(
-	score INTEGER
+CREATE FUNCTION grade(
+    score INTEGER
 )
 RETURNS INTEGER
+DETERMINISTIC
 BEGIN
-	RETURN score;
+    RETURN score;
 END
-//
+;;
 
 DELIMITER ;
 
 SELECT 
-	*,
-    Grade(score) AS `Grade`
-FROM Exam;
+    *,
+    grade(score) AS 'grade'
+FROM exam;
 
 
 
 --
--- Function for grading an exam.
+-- Function for grading an exam A-F, FX.
 --
-DELIMITER //
+DROP FUNCTION IF EXISTS grade;
+DELIMITER ;;
 
-DROP FUNCTION IF EXISTS Grade //
-CREATE FUNCTION Grade(
-	score INTEGER
+CREATE FUNCTION grade(
+    score INTEGER
 )
-RETURNS CHAR(1)
+RETURNS CHAR(2)
+DETERMINISTIC
 BEGIN
-	IF score >= 90 THEN
-		RETURN "A";
-	ELSEIF score >= 80 THEN
-		RETURN "B";
-	ELSEIF score >= 70 THEN
-		RETURN "C";
-	ELSEIF score >= 60 THEN
-		RETURN "D";
-	ELSEIF score >= 55 THEN
-		RETURN "E";
-	ELSEIF score >= 50 THEN
-		RETURN "FX";
-	END IF;
-    RETURN "F";
+    IF score >= 90 THEN
+        RETURN 'A';
+    ELSEIF score >= 80 THEN
+        RETURN 'B';
+    ELSEIF score >= 70 THEN
+        RETURN 'C';
+    ELSEIF score >= 60 THEN
+        RETURN 'D';
+    ELSEIF score >= 55 THEN
+        RETURN 'E';
+    ELSEIF score >= 50 THEN
+        RETURN 'FX';
+    END IF;
+    RETURN 'F';
 END
-//
+;;
 
 DELIMITER ;
 
 SELECT 
-	*,
-    Grade(score) AS `Grade`
-FROM Exam
-ORDER BY Grade;
+    *,
+    grade(score) AS 'grade'
+FROM exam
+ORDER BY grade;
 
 
 --
 -- Function for grading an exam U, 3-5.
 --
-DELIMITER //
+DROP FUNCTION IF EXISTS grade2;
+DELIMITER ;;
 
-DROP FUNCTION IF EXISTS Grade2 //
-CREATE FUNCTION Grade2(
-	score INTEGER
+CREATE FUNCTION grade2(
+    score INTEGER
 )
 RETURNS CHAR(1)
+DETERMINISTIC
 BEGIN
-	IF score >= 90 THEN
-		RETURN "5";
-	ELSEIF score >= 70 THEN
-		RETURN "4";
-	ELSEIF score >= 55 THEN
-		RETURN "3";
-	END IF;
-    RETURN "U";
+    IF score >= 90 THEN
+        RETURN '5';
+    ELSEIF score >= 70 THEN
+        RETURN '4';
+    ELSEIF score >= 55 THEN
+        RETURN '3';
+    END IF;
+    RETURN 'U';
 END
-//
+;;
 
 DELIMITER ;
 
 SELECT 
-	*,
-    Grade(score) AS `A-F, FX`,
-    Grade2(score) AS `U, 3-5`
-FROM Exam
+    *,
+    grade(score) AS 'A-F, FX',
+    grade2(score) AS 'U, 3-5'
+FROM exam
 ORDER BY score DESC;
+
+
+
+--
+-- Try out functions DETERMINISTIC and NOT DETERMINISTIC
+--
+DROP FUNCTION IF EXISTS combine;
+DELIMITER ;;
+
+CREATE FUNCTION combine (
+    str1 VARCHAR(6),
+    str2 VARCHAR(6)
+)
+RETURNS VARCHAR(12)
+DETERMINISTIC
+BEGIN
+    RETURN CONCAT(str1, str2);
+END
+;;
+
+DELIMITER ;
+
+SELECT combine('Mumin', 'troll');
+
+
+
+DROP FUNCTION IF EXISTS time_of_the_day;
+DELIMITER ;;
+
+CREATE FUNCTION time_of_the_day()
+RETURNS DATETIME
+NOT DETERMINISTIC NO SQL
+BEGIN
+    RETURN NOW();
+END
+;;
+
+DELIMITER ;
+
+SELECT time_of_the_day();
 
 
 
@@ -119,42 +162,4 @@ ORDER BY score DESC;
 -- Admin
 --
 SHOW FUNCTION STATUS;
-SHOW CREATE FUNCTION Grade; 
-
-
-
---
--- Try out functions DETERMINISTIC and NOT DETERMINISTIC
---
-DELIMITER //
-
-DROP FUNCTION IF EXISTS Combine //
-CREATE FUNCTION Combine (
-	str1 VARCHAR(6),
-    str2 VARCHAR(6)
-)
-RETURNS VARCHAR(12) DETERMINISTIC
-BEGIN
-	RETURN CONCAT(str1, str2);
-END
-//
-
-DELIMITER ;
-
-SELECT Combine("Mumin", "troll");
-
-
-
-DELIMITER //
-
-DROP FUNCTION IF EXISTS CurrentTime //
-CREATE FUNCTION CurrentTime ()
-RETURNS DATETIME NOT DETERMINISTIC
-BEGIN
-	RETURN NOW();
-END
-//
-
-DELIMITER ;
-
-SELECT CurrentTime();
+SHOW CREATE FUNCTION grade; 
--- example/sql/index.sql
+++ example/sql/index.sql
@@ -2,29 +2,29 @@
 -- https://dbwebb.se/kunskap/index-och-prestanda-i-mysql
 -- Example
 -- 
-DROP TABLE IF EXISTS `Course`;
-CREATE TABLE `Course`
+DROP TABLE IF EXISTS `course`;
+CREATE TABLE `course`
 (
-	`code` CHAR(6),
-	`nick` CHAR(12),
+    `code` CHAR(6),
+    `nick` CHAR(12),
     `points` DECIMAL(3, 1),
-	`name` VARCHAR(60)
+    `name` VARCHAR(60)
 );
 
-DELETE FROM Course;
-INSERT INTO Course
+DELETE FROM course;
+INSERT INTO course
 VALUES
-	("DV1531", "python",     7.5, "Programmering och Problemlösning med Python"),
-	("PA1439", "htmlphp",    7.5, "Webbteknologier"),
-	("DV1561", "javascript", 7.5, "Programmering med JavaScript"),
-	("PA1436", "design",     7.5, "Teknisk webbdesign och användbarhet"),
-	("DV1547", "linux",      7.5, "Programmera webbtjänster i Linux"),
-	("PA1437", "oopython",   7.5, "Objektorienterad design och programmering med Python"),
-	("DV1546", "webapp",     7.5, "Webbapplikationer för mobila enheter"),
-	("DV1506", "webgl",      7.5, "Spelteknik för webben"),
-	("PA1444", "dbjs",      10.0, "Webbprogrammering och databaser")
+    ('DV1531', 'python',     7.5, 'Programmering och Problemlösning med Python'),
+    ('PA1439', 'htmlphp',    7.5, 'Webbteknologier'),
+    ('DV1561', 'javascript', 7.5, 'Programmering med JavaScript'),
+    ('PA1436', 'design',     7.5, 'Teknisk webbdesign och användbarhet'),
+    ('DV1547', 'linux',      7.5, 'Programmera webbtjänster i Linux'),
+    ('PA1437', 'oopython',   7.5, 'Objektorienterad design och programmering med Python'),
+    ('DV1546', 'webapp',     7.5, 'Webbapplikationer för mobila enheter'),
+    ('DV1506', 'webgl',      7.5, 'Spelteknik för webben'),
+    ('PA1444', 'dbjs',      10.0, 'Webbprogrammering och databaser')
 ;
 
-SELECT * FROM Course;
+SELECT * FROM course;
 
-EXPLAIN select * from Course;
+EXPLAIN select * from course;
