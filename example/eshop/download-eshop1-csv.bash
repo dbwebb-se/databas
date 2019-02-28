c4980ec73145bebba8835fb927c610042c0d5318
Mikael Roos <mikael.t.h.roos@gmail.com>
2019-02-28 08:10:31 +0100

    Adding csv for eshop1

---
+++ example/eshop/download-eshop1-csv.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+url="https://docs.google.com/spreadsheets/d/1yz0-C1SFYvNw_mN5CrZd9QrjKUXqKv3OhSlGUci8Mls/gviz/tq?tqx=out:csv&sheet"
+
+for target in kund produkt kategori produkt2kategori lager produkt2lager; do
+    printf "%s\n" "$target"
+    curl --silent "$url=$target" > $target.csv
+done
+
+ls -l *.csv
+file *.csv
