ca077e86811d4499b1b776441882a07f6691b9b4
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-01-28 16:05:58 +0100

    Add terminal at end of inspect kmom01

---
+++ .dbwebb/inspect-src/kmom.d/kmom01/50_terminal1-bash.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# Script run for specific kmom (within docker).
+#
+# Available (and usable) data:
+#   $KMOM
+#   $ACRONYM
+#   $COURSE_REPO
+#
+cd me || exit
+e() { exit; }
+export -f e
+
+echo "[$ACRONYM] Do manual stuff, if needed (write e/exit to exit)?"
+ls -l
+bash
