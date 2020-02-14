430f6c8d91116193974e8285332582eb697469d5
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-02-14 09:22:43 +0100

    Add example on console table

---
+++ example/nodejs/table/console-table-printer.js
@@ -0,0 +1,73 @@
+/**
+ * Show off using https://www.npmjs.com/package/console-table-printer
+ */
+"use strict";
+
+const { printTable } = require('console-table-printer');
+const { Table } = require('console-table-printer');
+
+// Prepare a dice hand to hold the dices (its an array)
+let hand = [];
+
+// Add the dices to the dice hand and roll them once
+for (let i=0; i<5; i++) {
+    hand[i] = {
+        roll: i,
+        value: Math.round(Math.random() * 6 + 1)
+    };
+}
+
+// A array
+printTable(hand);
+
+//Create a table
+const testCases = [
+  { index: 3, text: 'I would like some gelb bananen bitte', value: 100 },
+  { index: 4, text: 'I hope batch update is working', value: 300 },
+];
+ 
+//print
+printTable(testCases);
+
+
+
+// A table instance
+//Create a table
+let p = new Table();
+ 
+//add rows with color
+p.addRow({ index: 1, text: 'red wine please', value: 10.212 });
+p.addRow({ index: 2, text: 'green gemuse please', value: 20.0 });
+p.addRows([
+  //adding multiple rows are possible
+  { index: 3, text: 'gelb bananen bitte', value: 100 },
+  { index: 4, text: 'update is working', value: 300 },
+]);
+ 
+//print
+p.printTable();
+
+
+
+// Colors
+p = new Table();
+p.addRow({ index: 1, text: 'red wine', value: 10.212 }, { color: 'red' });
+p.addRow({ index: 2, text: 'green gemuse', value: 20.0 }, { color: 'green' });
+p.addRow({ index: 3, text: 'gelb bananen', value: 100 }, { color: 'yellow' });
+p.printTable();
+
+
+
+// Create table object with control
+p = new Table({
+  style: 'fatBorder', //style of border of the table
+  columns: [
+    { name: 'column1', alignment: 'left' }, //with alignment
+    { name: 'column2', alignment: 'right' },
+    { name: 'column3' },
+  ],
+});
+p.addRow({ column1: 1, column2: 2, column3: 3});
+p.addRow({ column1: 4, column2: 5, column3: 6});
+p.addRow({ column1: 7, column2: 8, column3: 9});
+p.printTable();
---
+++ example/nodejs/table/console_table.js
@@ -0,0 +1,36 @@
+/**
+ * Show off using console.table()
+ */
+"use strict";
+
+// Prepare a dice hand to hold the dices (its an array)
+let hand = [];
+
+// Add the dices to the dice hand and roll them once
+for (let i=0; i<5; i++) {
+    hand[i] = Math.round(Math.random() * 6 + 1);
+}
+
+// A array
+console.table(hand);
+
+// Using header values
+console.table([
+    { a: 1, b: 'Y' },
+    { a: 'Z', b: 2 }
+]);
+
+// A larger table
+const structDatas = [
+  { handler: 'http', endpoint: 'http://localhost:3000/path', method: 'ALL' },
+  { handler: 'event', endpoint: 'http://localhost:3000/event', method: 'POST' },
+  { handler: 'GCS', endpoint: 'http://localhost:3000/GCS', method: 'POST' }
+];
+console.table(structDatas);
+
+// Table with header in first column
+const array = [{myId: 42, name: 'John', color: 'red'}, {myId: 1337, name: 'Jane', color: 'blue'}];
+
+const transformed = array.reduce((acc, {myId, ...x}) => { acc[myId] = x; return acc}, {});
+console.log(transformed);
+console.table(transformed);
