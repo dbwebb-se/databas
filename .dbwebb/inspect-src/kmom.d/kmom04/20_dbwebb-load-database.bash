2270503b099cfb01a918389166650f4644413ec7
Mikael Roos <mikael.t.h.roos@gmail.com>
2020-03-09 09:00:19 +0100

    inspect kmom04

--- .dbwebb/inspect-src/kmom.d/kmom04/20_dbwebb-load-database.bash
+++ .dbwebb/inspect-src/kmom.d/kmom04/20_dbwebb-load-database.bash
@@ -2,12 +2,12 @@
 
 set -e
 
-echo "mysql -udbwebb dbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql"
+echo "mysql -udbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql"
 echo "mysql -udbwebb dbwebb < example/sql/transaction_ddl.sql"
 echo "---"
 
-echo "Executing: mysql -udbwebb dbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql"
-mysql -udbwebb dbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql
+echo "Executing: mysql -udbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql"
+mysql -udbwebb < example/sql/inspect/setup_dbwebb_no_drop.sql
 
 echo "Executing: mysql -udbwebb dbwebb < example/sql/transaction_ddl.sql"
 mysql -udbwebb dbwebb < example/sql/transaction_ddl.sql
