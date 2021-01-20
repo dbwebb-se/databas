a7526c2097f8017fbb5b9957101da639bcae2d61
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-01-20 15:30:35 +0100

    Add example on concurrency

---
+++ example/nodejs/async-await/index6.js
@@ -0,0 +1,21 @@
+/**
+ * A test program to show off async and await.
+ *
+ * @author Mitt Namn
+ */
+"use strict";
+
+const fs = require("fs");
+const util = require("util");
+const readFile = util.promisify(fs.readFile);
+
+const path = "file.txt";
+
+console.info("1) Program is starting up!");
+
+readFile(path, "utf-8")
+.then(data => { console.info(data); })
+.then(() => { console.info("3) End of the program!"); })
+.catch(error => {console.error(error)});
+
+// console.info("3) End of the program!");
