44ec2cb5a09d83307d627fbab8272800b6130705
Mikael Roos <mikael.t.h.roos@gmail.com>
2021-02-18 14:43:05 +0100

    Uppdaterade SQL för tidsstämplar

--- example/sql/timestamp.sql
+++ example/sql/timestamp.sql
@@ -4,7 +4,8 @@
 DROP TABLE IF EXISTS t1;
 
 CREATE TABLE t1 (
-    id CHAR(2) PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    value CHAR(2),
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated TIMESTAMP DEFAULT NULL
                              ON UPDATE CURRENT_TIMESTAMP,
@@ -14,20 +15,37 @@ CREATE TABLE t1 (
 --
 -- Add some rows
 --
-INSERT INTO t1 (id) VALUES
-    (1), (2), (3);
+INSERT INTO t1 (value) VALUES
+    ('A'), ('B'), ('C');
 
 SELECT * FROM t1;
 
 --
 -- Show off how ON UPDATE works
 --
-UPDATE t1 SET id = 11 WHERE id = 1;
+UPDATE t1 SET value = 'Bb' WHERE id = 2;
+UPDATE t1 SET value = 'Cc' WHERE id = 3;
+
 SELECT * FROM t1;
 
 --
 -- Show the concept of "soft delete".
 --
-UPDATE t1 SET deleted = NOW() WHERE id = 11;
+UPDATE t1 SET deleted = NOW() WHERE id = 3;
+
 SELECT * FROM t1;
+
+
+--
+-- Show rows depending on status
+--
+SELECT * FROM t1 WHERE created IS NOT NULL;
+SELECT * FROM t1 WHERE updated IS NOT NULL;
 SELECT * FROM t1 WHERE deleted IS NULL;
+SELECT * FROM t1 WHERE deleted IS NOT NULL;
+
+SELECT * FROM t1
+WHERE
+    deleted IS NULL
+    AND updated IS NOT NULL
+;
