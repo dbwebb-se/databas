f2ae305c49039fd7ec8c36c02ea7fe035e6f6f2b
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-02-10 17:15:40 +0100

    Add example with readline/promises

---
+++ example/nodejs/mysql/search_await_node17.js
@@ -0,0 +1,108 @@
+/**
+ * Show teachers and their departments.
+ * This example uses Readline promises API that was introduced in Node 17.
+ * https://nodejs.org/api/readline.html#promises-api
+ */
+"use strict";
+
+const mysql  = require("promise-mysql");
+const config = require("./config.json");
+
+// Read from commandline
+const readline = require("readline/promises");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+
+/**
+ * Main function.
+ *
+ * @async
+ * @returns void
+ */
+(async function() {
+    const db = await mysql.createConnection(config);
+    let str;
+    let search;
+
+    search = await rl.question("What to search for? ");
+    str = await searchTeachers(db, search);
+    console.info(str);
+
+    rl.close();
+    db.end();
+})();
+
+
+
+/**
+ * Output resultset as formatted table with details on a teacher.
+ *
+ * @async
+ * @param {connection} db     Database connection.
+ * @param {string}     search String to search for.
+ *
+ * @returns {string} Formatted table to print out.
+ */
+async function searchTeachers(db, search) {
+    let sql;
+    let res;
+    let str;
+    let like = `%${search}%`;
+
+    console.info(`Searching for: ${search}`);
+
+    sql = `
+        SELECT
+            akronym,
+            fornamn,
+            efternamn,
+            avdelning,
+            lon
+        FROM larare
+        WHERE
+            akronym LIKE ?
+            OR fornamn LIKE ?
+            OR efternamn LIKE ?
+            OR avdelning LIKE ?
+            OR lon = ?
+        ORDER BY akronym;
+    `;
+    res = await db.query(sql, [like, like, like, like, search]);
+    str = teacherAsTable(res);
+    return str;
+}
+
+
+
+/**
+ * Output resultset as formatted table with details on a teacher.
+ *
+ * @param {RowDataPacket} res Resultset with details on a teacher.
+ *
+ * @returns {string} Formatted table to print out.
+ */
+function teacherAsTable(res) {
+    let str;
+
+    str  = "+-----------+---------------------+-----------+----------+\n";
+    str += "| Akronym   | Namn                | Avdelning |   Lön    |\n";
+    str += "|-----------|---------------------|-----------|----------|\n";
+    for (const row of res) {
+        str += "| ";
+        str += row.akronym.padEnd(10);
+        str += "| ";
+        str += (row.fornamn + " " + row.efternamn).padEnd(20);
+        str += "| ";
+        str += row.avdelning.padEnd(10);
+        str += "| ";
+        str += row.lon.toString().padStart(8);
+        str += " |\n";
+    }
+    str += "+-----------+---------------------+-----------+----------+\n";
+
+    return str;
+}
