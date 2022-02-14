fcab0bbf145c6e83ad60c867ee7ccf0288503549
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-02-14 22:47:24 +0100

    Add more users to docker instance

--- example/sql.d/mariadb/01_setup.sql
+++ example/sql.d/mariadb/01_setup.sql
@@ -9,36 +9,21 @@ CREATE DATABASE IF NOT EXISTS skolan;
 CREATE DATABASE IF NOT EXISTS dbwebb;
 
 -- Create the users
-CREATE USER IF NOT EXISTS 'user'@'%'
-    IDENTIFIED
-    -- WITH mysql_native_password
-    BY 'pass'
-;
-
-CREATE USER IF NOT EXISTS 'dbwebb'@'%'
-    IDENTIFIED
-    -- WITH mysql_native_password
-    BY 'pass'
-;
-
-CREATE USER IF NOT EXISTS 'maria'@'%'
-    IDENTIFIED
-    -- WITH mysql_native_password
-    BY 'pass'
-;
+CREATE USER IF NOT EXISTS 'user'@'%'           IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'user'@'localhost'   IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'dbwebb'@'%'         IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'dbwebb'@'localhost' IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'maria'@'%'          IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'maria'@'localhost'  IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'dbadm'@'%'          IDENTIFIED BY 'pass';
+CREATE USER IF NOT EXISTS 'dbadm'@'localhost'  IDENTIFIED BY 'pass';
 
 -- Grant the user all privilegies on the database.
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'user'@'%'
-;
-
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'dbwebb'@'%'
-;
-
-GRANT ALL PRIVILEGES
-    ON *.*
-    TO 'maria'@'%'
-;
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'user'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'dbwebb'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'dbwebb'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'maria'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'maria'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'dbadm'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'dbadm'@'localhost';
