aa74b664f9c438ba01fc9002ced1f944e302b191
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-03-15 16:25:49 +0100

    Fix grading kmom06

--- .dbwebb/inspect-src/kmom.d/kmom06/20_dbwebb-load-database.bash
+++ .dbwebb/inspect-src/kmom.d/kmom06/20_dbwebb-load-database.bash
@@ -4,15 +4,10 @@ set -e
 
 target="eshop"
 
-mysql -udbwebb < example/sql/inspect/setup_eshop.sql
-mysql -udbwebb $target < me/kmom06/eshop2/sql/eshop/backup.sql
-mysql -udbwebb $target < example/sql/list_proc_func_trig.sql
+mariadb -udbwebb < example/sql/inspect/setup_eshop.sql
+mariadb -udbwebb $target < me/kmom06/eshop2/sql/eshop/backup.sql
+mariadb -udbwebb $target < example/sql/list_proc_func_trig.sql
 
-echo "SHOW TABLES;"
-mysql -udbwebb -t -e "SHOW TABLES;" $target
-
-echo "SELECT * FROM v_proc;"
-mysql -udbwebb -t -e "SELECT * FROM v_proc;" $target
-
-echo "SELECT * FROM v_trig;"
-mysql -udbwebb -t -e "SELECT * FROM v_trig;" $target
+mariadb -udbwebb -t -e "SHOW TABLES;" $target
+mariadb -udbwebb -t -e "SELECT * FROM v_proc;" $target
+mariadb -udbwebb -t -e "SELECT * FROM v_trig;" $target
