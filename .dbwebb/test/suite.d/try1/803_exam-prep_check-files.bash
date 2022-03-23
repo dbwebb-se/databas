9e91b6881500a14b1519001304ea4fe258e88a92
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-03-23 23:53:19 +0100

    Add dbwebb test

---
+++ .dbwebb/test/suite.d/try1/803_exam-prep_check-files.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+. ".dbwebb/inspect-src/kmom.d/functions.bash"
+
+TARGET_DIR="me/kmom10/try1"
+CHECK_FILES="config/db/exam.json doc/er.pdf package.json index.js cli.js sql/exam/backup.sql sql/exam/setup.sql sql/exam/ddl.sql sql/exam/insert.sql sql/exam/dml.sql "
+
+cd $TARGET_DIR || exit 1
+
+all=0
+fail=0
+for file in $CHECK_FILES; do
+    (( all++ ))
+    if [[ ! -f "$file" ]]; then
+        printf "Missing file '$file'.\n"
+        (( fail++ ))
+    fi
+done
+
+doLog $fail "$TARGET_DIR: check files ("$(( all-fail ))"/$all)"
