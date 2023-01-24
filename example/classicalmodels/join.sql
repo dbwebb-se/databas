96b211ddd8bf6153504a28c949cbb21ca4916b94
Mikael Roos <mikael.t.h.roos@gmail.com>
2023-01-24 14:59:06 +0100

    After class

--- example/classicalmodels/join.sql
+++ example/classicalmodels/join.sql
@@ -10,10 +10,60 @@ SHOW COLUMNS FROM offices;
 SELECT * FROM employees;
 SELECT * FROM offices;
 
+SELECT
+	o.city,
+    o.country,
+    e.employeeNumber,
+    e.jobTitle,
+    e.firstName,
+    e.lastName
+FROM offices AS o
+	JOIN employees AS e
+		ON o.officeCode = e.officeCode
+;
+
+
 -- Create a report of all employees and their boss
+SELECT
+	o.city,
+    o.country,
+    e.employeeNumber,
+    e.jobTitle,
+    e.firstName,
+    e.lastName,
+    e.reportsTo,
+    CONCAT(boss.jobTitle, ' ', boss.firstName, ' ', boss.lastName) AS 'Boss'
+FROM offices AS o
+	JOIN employees AS e
+		ON o.officeCode = e.officeCode
+	JOIN employees AS boss
+		ON e.reportsTo = boss.employeeNumber
+;
+
+
 
 -- Create a report showing how many customers each salesreps have
 SELECT * FROM customers;
+
+SELECT
+	o.city,
+    o.country,
+    e.employeeNumber,
+    e.jobTitle,
+    e.firstName,
+    e.lastName,
+    COUNT(c.customerName) AS 'Customers'
+FROM offices AS o
+	JOIN employees AS e
+		ON o.officeCode = e.officeCode
+	JOIN customers As c
+	 	ON c.salesRepEmployeeNumber = e.employeeNumber
+GROUP BY
+	o.city -- e.employeeNumber
+ORDER BY
+	COUNT(c.customerName) DESC -- 'Customers'
+;
+
 
 -- Create a report showing which customer is connected to which office.
 -- Which office have the most customers?
