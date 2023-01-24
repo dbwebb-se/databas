c2b8f789e67cf3393128daddbeb96478bdf97d33
Mikael Roos <mikael.t.h.roos@gmail.com>
2023-01-24 14:10:58 +0100

    Add SQL from zoom

--- example/lecture/2023-lp3/kmom02.md
+++ example/lecture/2023-lp3/kmom02.md
@@ -1,6 +1,7 @@
 Kmom02 genomgång
 =========================
 
+Kolla snabbt igenom vad som händer i kmom02.
 
 
 Hur inlämningen och rättning fungerar
@@ -9,14 +10,12 @@ Hur inlämningen och rättning fungerar
 `dbwebb test kmom01`
 
 
-
 Använd databasen "world"
 -------------------------
 
 Använd databasen "world" och gör en tripple join.
 
 `example/world/join.sql`
-
 
 
 Använd databasen "classicalmodels"
---
+++ example/world/join_from_class.sql
@@ -0,0 +1,61 @@
+SHOW DATABASES;
+USE world;
+SHOW TABLES;
+
+SHOW COLUMNS FROM city;
+SHOW COLUMNS FROM country;
+SHOW COLUMNS FROM countrylanguage;
+
+SELECT * FROM city LIMIT 10;
+SELECT * FROM country LIMIT 10;
+SELECT * FROM countrylanguage LIMIT 10;
+
+-- Detaljer om Sverige
+SELECT * FROM country WHERE Name = 'Sweden';
+SELECT * FROM country WHERE LocalName = 'Sverige';
+SELECT * FROM country WHERE Code = 'SWE';
+
+-- Vilka städer ligger i Sverige
+SELECT * FROM city WHERE CountryCode = 'SWE';
+
+-- Vilka språk pratar man i Sverige
+SELECT * FROM countrylanguage WHERE CountryCode = 'SWE';
+SELECT * FROM countrylanguage WHERE CountryCode = 'SWE' ORDER BY Percentage DESC;
+
+-- Vilken är Sveriges huvudstad?
+SELECT
+	co.Name AS 'Country',
+    ci.Name AS 'Capital'
+FROM country AS co
+	JOIN city AS ci
+		ON ci.ID = co.Capital
+WHERE
+	-- co.Code = 'SWE'
+	-- co.Code IN ('SWE', 'FIN')
+    co.Region LIKE 'Nordic%'
+;
+
+-- Visa alla nordiska länder med deras huvudstäder samt vilka officiella språk som talas i dessa länder.
+SELECT * FROM countrylanguage WHERE CountryCode = 'SWE' ORDER BY Percentage DESC;
+
+SELECT
+	co.Name AS 'Country',
+    ci.Name AS 'Capital',
+    COUNT(lang.Language) AS 'Antal språk',
+    GROUP_CONCAT(
+		CONCAT(lang.Language, ' (', lang.Percentage, ')')
+        ORDER BY lang.Percentage DESC
+	) AS 'Language'
+FROM country AS co
+	JOIN city AS ci
+		ON ci.ID = co.Capital
+	JOIN countrylanguage AS lang
+		ON lang.CountryCode = co.Code
+WHERE
+	-- co.Code = 'SWE'
+	-- Code IN ('SWE', 'FIN')
+    co.Region LIKE 'Nordic%'
+    AND lang.IsOfficial = 'T'
+GROUP BY
+	co.Name
+;
