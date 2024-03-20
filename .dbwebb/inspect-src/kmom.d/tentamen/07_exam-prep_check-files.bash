b32c55a9efeec62dcbca17fd1068437af0ce73df
Mikael Roos <mikael.t.h.roos@gmail.com>
2024-03-20 22:42:46 +0100

    Done preparing exam

--- .dbwebb/inspect-src/kmom.d/tentamen/07_exam-prep_check-files.bash
+++ .dbwebb/inspect-src/kmom.d/tentamen/07_exam-prep_check-files.bash
@@ -2,7 +2,7 @@
 . ".dbwebb/inspect-src/kmom.d/functions.bash"
 
 TARGET_DIR="me/tentamen"
-CHECK_FILES="config/db/exam.json doc/er.pdf package.json index.js cli.js sql/exam/backup.sql sql/exam/setup.sql sql/exam/ddl.sql sql/exam/insert.sql sql/exam/dml.sql "
+CHECK_FILES="config/db/exam.json doc/er.pdf index.js cli.js sql/exam/backup.sql sql/exam/setup.sql sql/exam/ddl.sql sql/exam/insert.sql sql/exam/dml.sql "
 
 cd $TARGET_DIR || exit 1
 
